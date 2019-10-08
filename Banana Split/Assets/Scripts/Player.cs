ccbe6bc3280e2745212b685aab64cda38ee3bf24
kpeenee <43548093+kpeenee@users.noreply.github.com>
2019-10-08 11:31:27 +0100

    Player movement

---
+++ Banana Split/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [Header("Player moovement")]
+    [SerializeField] float moveSpeed = 5f;
+
+    void Start()
+    {
+        //Get rigidbody on player
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float inputHorizon = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector3(inputHorizon * moveSpeed, rb.velocity.y, 0);
+    }
+}
