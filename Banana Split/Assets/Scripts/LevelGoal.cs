0199ef97d51dadd352adfc66e6227e2be4e760cd
kpeenee <43548093+kpeenee@users.noreply.github.com>
2019-10-12 13:26:06 +0100

    Level goal

---
+++ Banana Split/Assets/Scripts/LevelGoal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGoal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            FindObjectOfType<SceneLoader>().LoadNextLevel();
+        }
+    }
+}
