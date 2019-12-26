879e603c5b6a2902071fa4aa69ae7916ca5a2046
kpeenee <43548093+kpeenee@users.noreply.github.com>
2019-12-26 13:16:52 +0000

    Moving platforms

---
+++ Banana Split/Assets/MovingPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints = new List<Transform>();
+    [SerializeField] float speed;
+    int currentPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentPoint].position, Time.deltaTime * speed);
+        if(transform.position == waypoints[currentPoint].position)
+        {
+            currentPoint++;
+            if (currentPoint >= waypoints.Count)
+            {
+                currentPoint = 0;
+            }
+            
+        }
+    }
+}
